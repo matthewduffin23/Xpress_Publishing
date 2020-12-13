9ce210d23baba69af732d44677f382d45066dc89
matthewduffin23 <65633806+matthewduffin23@users.noreply.github.com>
2020-12-12 23:21:25 -0800

    final commit

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const express = require('express');
 const apiRouter = require('./api/api');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 /*const PORT = process.env.PORT || 4000; */
 
 app.use(bodyParser.json());
